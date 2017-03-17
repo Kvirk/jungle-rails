17c0b5c54bf02266ac3b8083deee92ddd2e29289
Kirat Virk <kirat_virk3@yahoo.ca>
2017-03-17 19:53:12 +0000

    added error messages

--- app/controllers/concerns/sessions_controller.rb
+++ app/controllers/concerns/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       redirect_to '/'
     else
     # If user's login doesn't work, send them back to the login form.
-      redirect_to '/login'
+      redirect_to '/login', flash: {notice: 'Email and/or password wrong!'}
     end
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
       session[:user_id] = user.id
       redirect_to '/'
     else
-      redirect_to '/signup'
+      redirect_to '/signup', flash: {notice: 'Email already use and/or password confirmation wrong!'}
     end
   end
 
