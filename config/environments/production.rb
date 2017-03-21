adc734eb03865cf98aff524cc0ac23c266bbb4e4
Kirat Virk <kirat_virk3@yahoo.ca>
2017-03-21 01:27:27 +0000

    readding secrets.yml with .env as variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,11 +5,12 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.mail.com",
    :port                 => 587,
-   :user_name            => ENV['gmail_username'],
-   :password             => ENV['gmail_password'],
+   :user_name            => ENV['GMAIL_USERNAME'],
+   :password             => ENV['GMAIL_PASSWORD'],
    :authentication       => "plain",
   :enable_starttls_auto => true
   }
+
 
   # Code is not reloaded between requests.
   config.cache_classes = true
